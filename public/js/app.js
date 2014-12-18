1feddf7e99a4ee68356365f4e2e00c5fee829e84
Eunsub Kim <eunsub@gmail.com>
2014-12-18 10:04:26 +0900

    blockly category tree

--- public/js/app.js
+++ public/js/app.js
@@ -17,6 +17,33 @@ _xml = function(prettify){
   return xml;
 };
 
+var reload_udf_blocks = function(items){
+
+  var $cat = $('category[name=Utils]');
+
+  // $('category[name=Utils] block').remove();
+  _.each(items, function(item){
+    var parser = new DOMParser();
+    var xml = parser.parseFromString(item.xml, "text/xml");
+
+    $(xml).find('block[type=procedures_defreturn],block[type=procedures_defnoreturn]').each(function(b){
+      var fname = $(this).find('> field[name]').text();
+      var args = $(this).find('mutation arg').map(function(e){ return $(this).attr('name'); }).toArray();
+      Blockly.register_function_block(fname, args, $(this).attr('type') == 'procedures_defreturn');
+
+      
+      $cat.find('block[type=udf_' + fname + ']').remove();
+      $cat.append('<block type="udf_'+fname+'"></block>');
+
+
+    });
+    Blockly.updateToolbox($('#toolbox').get(0));
+
+  });
+
+};
+
+
 var toggle_header_menu = function(){
   $('#header').toggle();
   if($('#header').is(':visible')){
--- public/js/ctrls/list_ctrl.js
+++ public/js/ctrls/list_ctrl.js
@@ -1,28 +1,4 @@
 
-var reload_udf_blocks = function(items){
-
-
-  $('category[name=UDF] block').remove();
-  _.each(items, function(item){
-    var parser = new DOMParser();
-    var xml = parser.parseFromString(item.xml, "text/xml");
-
-    $(xml).find('block[type=procedures_defreturn],block[type=procedures_defnoreturn]').each(function(b){
-      var fname = $(this).find('> field[name]').text();
-      var args = $(this).find('mutation arg').map(function(e){ return $(this).attr('name'); }).toArray();
-      Blockly.register_function_block(fname, args, $(this).attr('type') == 'procedures_defreturn');
-
-      var $cat = $('category[name=UDF]');
-      $cat.append('<block type="udf_'+fname+'"></block>');
-
-
-    });
-    Blockly.updateToolbox($('#toolbox').get(0));
-
-  });
-
-};
-
 var app = angular.module('centoAuthoring');
 app.controller('ListCtrl', function($scope, blocksStore, $http) {
 
