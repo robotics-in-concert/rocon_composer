622b314b242a24f57316edf2cabe64f6e86d3088
Eunsub Kim <eunsub@gmail.com>
2015-01-08 16:49:50 +0900

    load rapps before config editor

--- public/dist.js
+++ public/dist.js
@@ -2207,44 +2207,53 @@ function ConfigCtrl($scope, blocksStore, $http, $mi) {
 
 
 
-  this.schema = {
-    title: 'blockconfig',
-    type: "object",
-    properties: {
-      rapp: {type: 'string'},
-      uri: {type: 'string'},
-      timeout: {
-        type: 'integer',
-        default: 15000
-      },
-      remappings: { 
-        type: 'array',
-        format: 'table',
-        title: 'Remappings',
-        items: {
-          type: 'object',
-          properties: {
-            remap_from: {type: 'string'},
-            remap_to: {type: 'string'}
-          }
 
-        }
-      },
-      parameters: { 
-        type: 'array',
-        format: 'table',
-        title: 'Parameters',
-        items: {
-          type: 'object',
-          properties: {
-            key: {type: 'string'},
-            value: {type: 'string'}
-          }
+  this.schema = blocksStore.loadRapp().then(function(){
+    var schema = {
+      title: 'blockconfig',
+      type: "object",
+      properties: {
+        rapp: {type: 'string'},
+        uri: {type: 'string'},
+        timeout: {
+          type: 'integer',
+          default: 15000
+        },
+        remappings: { 
+          type: 'array',
+          format: 'table',
+          title: 'Remappings',
+          items: {
+            type: 'object',
+            properties: {
+              remap_from: {type: 'string'},
+              remap_to: {type: 'string'}
+            }
 
-        }
-      },
-    }
-  };
+          }
+        },
+        parameters: { 
+          type: 'array',
+          format: 'table',
+          title: 'Parameters',
+          items: {
+            type: 'object',
+            properties: {
+              key: {type: 'string'},
+              value: {type: 'string'}
+            }
+
+          }
+        },
+      }
+    };
+
+
+    return schema;
+  });
+  
+
+
   this.startval = null;
 
   if(Blockly.selected && Blockly.selected.extra_config){
