e1dc39778db1feb6686866865dbea7298b852bed
Eunsub Kim <eunsub@gmail.com>
2015-01-12 19:50:48 +0900

    create, join channel

--- io.js
+++ io.js
@@ -10,9 +10,28 @@ module.exports = function(io){
       socket.emit('blockly:workspace:locked');
     });
     socket.on('blockly:workspace:changed', function(e){
-      if(socket.host)
-        socket.broadcast.emit('blockly:workspace:changed', e);
+      if(socket.host && socket.channel){
+        socket.broadcast.to(socket.channel).emit('blockly:workspace:changed', e);
+      }
     });
+    socket.on('blockly:channel:create', function(e){
+      socket.host = true;
+      socket.channel = e.name;
+      socket.join(e.name);
+      return 'created';
+
+    });
+    socket.on('blockly:channel:join', function(e){
+      socket.host = false;
+      socket.channel = e.name;
+      socket.join(e.name);
+    });
+    socket.on('blockly:channel:leave', function(e){
+      socket.host = false;
+      socket.leave(socket.channel);
+      socket.channel = null;
+    });
+
 
 
 
