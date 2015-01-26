2ea45ea55046df2814f6a3d039cf59046dbc27d9
Eunsub Kim <eunsub@gmail.com>
2015-01-26 15:05:45 +0900

    block config in/out

--- public/bundle.js
+++ public/bundle.js
@@ -2142,7 +2142,7 @@ Blockly.JavaScript['defer'] = function(block) {
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{"../config":"/Users/eskim/current/cento_authoring/public/js/config.json"}],"/Users/eskim/current/cento_authoring/public/js/config.json":[function(require,module,exports){
-module.exports=module.exports=module.exports=module.exports=module.exports=module.exports=module.exports={
+module.exports={
   "action_color": 100,
   "undo_check_interval": 1000,
   "undo_max_size": 100
@@ -2181,91 +2181,16 @@ function ConfigCtrl($scope, blocksStore, $http, $mi) {
     ctrl.config.parameters.splice(idx, 1);
   };
 
-
-  this.schema = blocksStore.loadRapp().then(function(){
-    var schema = {
-      title: 'blockconfig',
-      type: "object",
-      properties: {
-        rapp: {type: 'string'},
-        uri: {type: 'string'},
-        timeout: {
-          type: 'integer',
-          default: 15000
-        },
-        remappings: { 
-          type: 'array',
-          format: 'table',
-          title: 'Remappings',
-          items: {
-            type: 'object',
-            properties: {
-              remap_from: {type: 'string'},
-              remap_to: {type: 'string'}
-            }
-
-          }
-        },
-        parameters: { 
-          type: 'array',
-          format: 'table',
-          title: 'Parameters',
-          items: {
-            type: 'object',
-            properties: {
-              key: {type: 'string'},
-              value: {type: 'string'}
-            }
-
-          }
-        },
-      }
-    };
-
-
-    return schema;
-  });
-  
-
-
-  this.startval = null;
-
   if(Blockly.selected && Blockly.selected.extra_config){
-    this.startval = Blockly.selected.extra_config;
+    this.config = Blockly.selected.extra_config;
   }
 
 
 
-  this.editor_options = {
-    disable_array_reorder: true,
-    disable_collapse: true,
-    disable_edit_json: true,
-    disable_properties: true
-  };
-
-
-
-
-
-  // var editor = this.editor = new JSONEditor($('#config-editor').get(0), {
-    // disable_array_reorder: true,
-    // disable_collapse: true,
-    // disable_edit_json: true,
-    // disable_properties: true,
-    // schema: 
-    
-  // });
-  // var default_value = editor.getValue();
-  // window.editor = editor;
-
-  this.onChange = function(data){
-    console.log(data);
-    ctrl.value = data;
-  };
 
   this.ok = function(){
     if(Blockly.selected){
-      Blockly.selected.extra_config = ctrl.value;
+      Blockly.selected.extra_config = ctrl.config;
     };
     $mi.dismiss();
 
@@ -3163,7 +3088,7 @@ module.exports = {
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{}],"/Users/eskim/current/cento_authoring/public/js/schema/service_form.json":[function(require,module,exports){
-module.exports=module.exports=module.exports=module.exports=module.exports=module.exports=module.exports={
+module.exports={
   "title": "Create Service",
   "type": "object",
   "properties": {
--- public/js/ctrls/config_ctrl.js
+++ public/js/ctrls/config_ctrl.js
@@ -30,91 +30,16 @@ function ConfigCtrl($scope, blocksStore, $http, $mi) {
     ctrl.config.parameters.splice(idx, 1);
   };
 
-
-  this.schema = blocksStore.loadRapp().then(function(){
-    var schema = {
-      title: 'blockconfig',
-      type: "object",
-      properties: {
-        rapp: {type: 'string'},
-        uri: {type: 'string'},
-        timeout: {
-          type: 'integer',
-          default: 15000
-        },
-        remappings: { 
-          type: 'array',
-          format: 'table',
-          title: 'Remappings',
-          items: {
-            type: 'object',
-            properties: {
-              remap_from: {type: 'string'},
-              remap_to: {type: 'string'}
-            }
-
-          }
-        },
-        parameters: { 
-          type: 'array',
-          format: 'table',
-          title: 'Parameters',
-          items: {
-            type: 'object',
-            properties: {
-              key: {type: 'string'},
-              value: {type: 'string'}
-            }
-
-          }
-        },
-      }
-    };
-
-
-    return schema;
-  });
-  
-
-
-  this.startval = null;
-
   if(Blockly.selected && Blockly.selected.extra_config){
-    this.startval = Blockly.selected.extra_config;
+    this.config = Blockly.selected.extra_config;
   }
 
 
 
-  this.editor_options = {
-    disable_array_reorder: true,
-    disable_collapse: true,
-    disable_edit_json: true,
-    disable_properties: true
-  };
-
-
-
-
-
-  // var editor = this.editor = new JSONEditor($('#config-editor').get(0), {
-    // disable_array_reorder: true,
-    // disable_collapse: true,
-    // disable_edit_json: true,
-    // disable_properties: true,
-    // schema: 
-    
-  // });
-  // var default_value = editor.getValue();
-  // window.editor = editor;
-
-  this.onChange = function(data){
-    console.log(data);
-    ctrl.value = data;
-  };
 
   this.ok = function(){
     if(Blockly.selected){
-      Blockly.selected.extra_config = ctrl.value;
+      Blockly.selected.extra_config = ctrl.config;
     };
     $mi.dismiss();
 
