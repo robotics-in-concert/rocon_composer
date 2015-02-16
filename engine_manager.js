aec0473d927a1b581094235dfcda50c2736de8a3
Eunsub Kim <eunsub@gmail.com>
2015-02-16 09:39:16 +0900

    engine manager as event emitter

--- engine_manager.js
+++ engine_manager.js
@@ -1,10 +1,14 @@
-var spawn = require('child_process').spawn;
+var spawn = require('child_process').spawn,
+  util = require('util'),
+  EventEmitter2 = require('eventemitter2').EventEmitter2;
 
 var EngineManager = function(io, options){
+  EventEmitter2.call(this, {wildcard: true});
   this.io = io;
   this.engine_processes = {};
   this.options = options;
 };
+util.inherits(EngineManager, EventEmitter2);
 
 
 EngineManager.prototype.startEngine = function(io, extras){
@@ -25,10 +29,44 @@ EngineManager.prototype.startEngine = function(io, extras){
   });
 
 
+  this._bindEvents(child);
   this.engine_processes[child.pid] = child;
 
   return child.pid;
 }
+
+
+
+EngineManager.prototype._bindEvents = function(child){
+  child.on('message', function(msg){
+    if(msg == 'engine_start_failed'){
+      logger.error('engine start failed');
+    }else if(msg == 'engine_started'){
+      logger.info('engine started');
+    }else if(msg == 'engine_ready'){
+      logger.info('engine ready')
+      // var workflows = argv.workflow;
+      // if(!_.isEmpty(workflows)){
+        // var col = db.collection('settings');
+        // col.findOne({key: 'cento_authoring_items'}, function(e, data){
+          // var items = data ? data.value.data : [];
+          // var items_to_load = _(items)
+            // .filter(function(i) { return _.contains(workflows, i.title); })
+            // .sortBy(function(i) { return _.indexOf(workflows, i.title); })
+            // .value();
+      
+          // global.childEngine.send({action: 'run', items: items_to_load});
+        // });
+
+      // }
+
+    }
+
+  });
+
+};
+
+
 
 
 
@@ -54,6 +92,33 @@ EngineManager.prototype.restartEngine = function(pid){
   });
   childEngine.send({action: 'stop'});
 };
+
+// global.childEngine.on('message', function(msg){
+
+  // if(msg == 'engine_start_failed'){
+    // logger.error('engine start failed');
+  // }else if(msg == 'engine_started'){
+    // logger.info('engine started');
+  // }else if(msg == 'engine_ready'){
+    // logger.info('engine ready')
+    // var workflows = argv.workflow;
+    // if(!_.isEmpty(workflows)){
+      // var col = db.collection('settings');
+      // col.findOne({key: 'cento_authoring_items'}, function(e, data){
+        // var items = data ? data.value.data : [];
+        // var items_to_load = _(items)
+          // .filter(function(i) { return _.contains(workflows, i.title); })
+          // .sortBy(function(i) { return _.indexOf(workflows, i.title); })
+          // .value();
+    
+        // global.childEngine.send({action: 'run', items: items_to_load});
+      // });
+
+    // }
+
+  // }
+
+// });
 
 
 
