f2b808e1ef10dc764766320a3e7ce5b5c23f6313
Eunsub Kim <eunsub@gmail.com>
2015-03-18 16:13:49 +0900

    ui blocks :art:

--- rocon_composer_blockly/public/bundle.js
+++ rocon_composer_blockly/public/bundle.js
@@ -2042,6 +2042,24 @@ Blockly.JavaScript['ros_service'] = function(block) {
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{"../config":15}],13:[function(require,module,exports){
 
+Blockly.Blocks['ui_create'] = {
+
+  init: function() {
+    this.setColour(30);
+    this.appendDummyInput().appendField('Create UI');
+    this.appendDummyInput().appendField(new Blockly.FieldTextInput('name', null), 'NAME')
+    this.setInputsInline(true);
+
+    this.appendStatementInput('COMPS')
+      .appendField("components")
+
+    this.setPreviousStatement(true);
+    return this.setNextStatement(true);
+  },
+
+};
+
+
 Blockly.Blocks['ui_horizontal'] = {
 
   init: function() {
@@ -2049,7 +2067,7 @@ Blockly.Blocks['ui_horizontal'] = {
     this.appendDummyInput().appendField('Layout(horizontal)' + name);
     this.setInputsInline(true);
 
-    this.appendStatementInput('components')
+    this.appendStatementInput('COMPS')
       .appendField("components")
 
     this.setPreviousStatement(true);
@@ -2065,7 +2083,7 @@ Blockly.Blocks['ui_vertical'] = {
     this.appendDummyInput().appendField('Layout(vertical)' + name);
     this.setInputsInline(true);
 
-    this.appendStatementInput('components')
+    this.appendStatementInput('COMPS')
       .appendField("components")
 
     this.setPreviousStatement(true);
@@ -2089,6 +2107,30 @@ Blockly.Blocks['ui_button'] = {
 
   },
 
+};
+Blockly.JavaScript['ui_button'] = function(block){
+  return 'layout.push({type: "button"});';
+};
+
+
+var _componentsMeta = function(block){
+  var code = Blockly.JavaScript.statementToCode(block, 'COMPS', Blockly.JavaScript.ORDER_NONE) || "''";
+  var meta_tpl = _.template('(function(layout){ <%= code %>; return layout; })([]);');
+  var meta = meta_tpl({code: code});
+  return JSON.stringify(eval(meta));
+
+};
+
+Blockly.JavaScript['ui_vertical'] = function(block){
+  return _.template('layout.push({type: "vertical", children: <%= meta %>});')({meta: _componentsMeta(block)});
+};
+Blockly.JavaScript['ui_horizontal'] = function(block){
+  return _.template('layout.push({type: "horizontal", children: <%= meta %>});')({meta: _componentsMeta(block)});
+};
+Blockly.JavaScript['ui_create'] = function(block){
+  var name = block.getFieldValue('NAME');
+  var tpl = _.template("$engine.createUI('<%= name%>', <%= meta %>);");
+  return tpl({name: name, meta: _componentsMeta(block)});
 };
 
 
--- rocon_composer_blockly/public/js/blocks/ui.js
+++ rocon_composer_blockly/public/js/blocks/ui.js
@@ -1,4 +1,22 @@
 
+Blockly.Blocks['ui_create'] = {
+
+  init: function() {
+    this.setColour(30);
+    this.appendDummyInput().appendField('Create UI');
+    this.appendDummyInput().appendField(new Blockly.FieldTextInput('name', null), 'NAME')
+    this.setInputsInline(true);
+
+    this.appendStatementInput('COMPS')
+      .appendField("components")
+
+    this.setPreviousStatement(true);
+    return this.setNextStatement(true);
+  },
+
+};
+
+
 Blockly.Blocks['ui_horizontal'] = {
 
   init: function() {
@@ -6,7 +24,7 @@ Blockly.Blocks['ui_horizontal'] = {
     this.appendDummyInput().appendField('Layout(horizontal)' + name);
     this.setInputsInline(true);
 
-    this.appendStatementInput('components')
+    this.appendStatementInput('COMPS')
       .appendField("components")
 
     this.setPreviousStatement(true);
@@ -22,7 +40,7 @@ Blockly.Blocks['ui_vertical'] = {
     this.appendDummyInput().appendField('Layout(vertical)' + name);
     this.setInputsInline(true);
 
-    this.appendStatementInput('components')
+    this.appendStatementInput('COMPS')
       .appendField("components")
 
     this.setPreviousStatement(true);
@@ -46,5 +64,29 @@ Blockly.Blocks['ui_button'] = {
 
   },
 
+};
+Blockly.JavaScript['ui_button'] = function(block){
+  return 'layout.push({type: "button"});';
+};
+
+
+var _componentsMeta = function(block){
+  var code = Blockly.JavaScript.statementToCode(block, 'COMPS', Blockly.JavaScript.ORDER_NONE) || "''";
+  var meta_tpl = _.template('(function(layout){ <%= code %>; return layout; })([]);');
+  var meta = meta_tpl({code: code});
+  return JSON.stringify(eval(meta));
+
+};
+
+Blockly.JavaScript['ui_vertical'] = function(block){
+  return _.template('layout.push({type: "vertical", children: <%= meta %>});')({meta: _componentsMeta(block)});
+};
+Blockly.JavaScript['ui_horizontal'] = function(block){
+  return _.template('layout.push({type: "horizontal", children: <%= meta %>});')({meta: _componentsMeta(block)});
+};
+Blockly.JavaScript['ui_create'] = function(block){
+  var name = block.getFieldValue('NAME');
+  var tpl = _.template("$engine.createUI('<%= name%>', <%= meta %>);");
+  return tpl({name: name, meta: _componentsMeta(block)});
 };
 
