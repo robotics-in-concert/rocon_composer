effcc3e68725ef4d5c21cafeac626b0d3b14c010
Eunsub Kim <eunsub@gmail.com>
2015-03-23 11:55:39 +0900

    add text control, fix button control

--- rocon_composer_blockly/public/bundle.js
+++ rocon_composer_blockly/public/bundle.js
@@ -2091,6 +2091,23 @@ Blockly.Blocks['ui_vertical'] = {
   },
 
 };
+Blockly.Blocks['ui_text'] = {
+
+  init: function() {
+    this.setColour(30);
+    this.appendDummyInput().appendField('Text' + name);
+
+    this.appendDummyInput().appendField('name:')
+      .appendField(new Blockly.FieldTextInput('name', null), 'NAME');
+
+    this.appendValueInput('TEXT').appendField('text:');
+    this.setInputsInline(true);
+    this.setPreviousStatement(true);
+    return this.setNextStatement(true);
+
+  },
+
+};
 
 Blockly.Blocks['ui_button'] = {
 
@@ -2098,8 +2115,10 @@ Blockly.Blocks['ui_button'] = {
     this.setColour(30);
     this.appendDummyInput().appendField('Button' + name);
 
-    this.appendValueInput('NAME').appendField('name : ');
-    this.appendValueInput('TEXT').appendField('text : ');
+    this.appendDummyInput().appendField('name:')
+      .appendField(new Blockly.FieldTextInput('name', null), 'NAME');
+
+    this.appendValueInput('TEXT').appendField('text:');
     this.setInputsInline(true);
     this.setPreviousStatement(true);
     return this.setNextStatement(true);
@@ -2109,7 +2128,28 @@ Blockly.Blocks['ui_button'] = {
 
 };
 Blockly.JavaScript['ui_button'] = function(block){
-  return 'layout.push({type: "button"});';
+  var text = Blockly.JavaScript.valueToCode(block, 'TEXT', Blockly.JavaScript.ORDER_NONE) || "''";
+  var name = block.getFieldValue('NAME');
+
+  console.log(name);
+
+  var code = _.template('layout.push({type: "button", name: "<%= name %>", text: <%= text %>});')({
+    text: text,
+    name: name
+  });
+  return code;
+};
+Blockly.JavaScript['ui_text'] = function(block){
+  var text = Blockly.JavaScript.valueToCode(block, 'TEXT', Blockly.JavaScript.ORDER_NONE) || "''";
+  var name = block.getFieldValue('NAME');
+
+  console.log(name);
+
+  var code = _.template('layout.push({type: "text", name: "<%= name %>", text: <%= text %>});')({
+    text: text,
+    name: name
+  });
+  return code;
 };
 
 
--- rocon_composer_blockly/public/js/blocks/ui.js
+++ rocon_composer_blockly/public/js/blocks/ui.js
@@ -48,6 +48,23 @@ Blockly.Blocks['ui_vertical'] = {
   },
 
 };
+Blockly.Blocks['ui_text'] = {
+
+  init: function() {
+    this.setColour(30);
+    this.appendDummyInput().appendField('Text' + name);
+
+    this.appendDummyInput().appendField('name:')
+      .appendField(new Blockly.FieldTextInput('name', null), 'NAME');
+
+    this.appendValueInput('TEXT').appendField('text:');
+    this.setInputsInline(true);
+    this.setPreviousStatement(true);
+    return this.setNextStatement(true);
+
+  },
+
+};
 
 Blockly.Blocks['ui_button'] = {
 
@@ -55,8 +72,10 @@ Blockly.Blocks['ui_button'] = {
     this.setColour(30);
     this.appendDummyInput().appendField('Button' + name);
 
-    this.appendValueInput('NAME').appendField('name : ');
-    this.appendValueInput('TEXT').appendField('text : ');
+    this.appendDummyInput().appendField('name:')
+      .appendField(new Blockly.FieldTextInput('name', null), 'NAME');
+
+    this.appendValueInput('TEXT').appendField('text:');
     this.setInputsInline(true);
     this.setPreviousStatement(true);
     return this.setNextStatement(true);
@@ -66,7 +85,28 @@ Blockly.Blocks['ui_button'] = {
 
 };
 Blockly.JavaScript['ui_button'] = function(block){
-  return 'layout.push({type: "button"});';
+  var text = Blockly.JavaScript.valueToCode(block, 'TEXT', Blockly.JavaScript.ORDER_NONE) || "''";
+  var name = block.getFieldValue('NAME');
+
+  console.log(name);
+
+  var code = _.template('layout.push({type: "button", name: "<%= name %>", text: <%= text %>});')({
+    text: text,
+    name: name
+  });
+  return code;
+};
+Blockly.JavaScript['ui_text'] = function(block){
+  var text = Blockly.JavaScript.valueToCode(block, 'TEXT', Blockly.JavaScript.ORDER_NONE) || "''";
+  var name = block.getFieldValue('NAME');
+
+  console.log(name);
+
+  var code = _.template('layout.push({type: "text", name: "<%= name %>", text: <%= text %>});')({
+    text: text,
+    name: name
+  });
+  return code;
 };
 
 
