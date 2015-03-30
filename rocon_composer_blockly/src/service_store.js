229328ea9679eeeddb3be29eef2613c09c8d1687
Eunsub Kim <eunsub@gmail.com>
2015-03-30 15:57:28 +0900

    fix export :bug:

--- rocon_composer_blockly/src/service_store.js
+++ rocon_composer_blockly/src/service_store.js
@@ -1,4 +1,5 @@
 var  R = require('ramda'),
+  _ = require('lodash'),
   Promise = require('bluebird'),
   glob = Promise.promisify(require('glob')),
   fs = Promise.promisifyAll(require('fs')),
@@ -17,6 +18,7 @@ var ServiceStore = function(options){
   // options
   // - ros_root
   this.options = options;
+  this.repo_root = Path.join(os.tmpdir(), "service_repository");
 
 };
 
@@ -32,7 +34,7 @@ var _to_colon_sep = function(obj){
 
 
 ServiceStore.prototype._withRepo = function(){
-  var repo_root = Path.join(os.tmpdir(), "service_repository");
+  var repo_root = this.repo_root;
   console.log("tmp repo root", repo_root);
 
 
@@ -92,89 +94,103 @@ ServiceStore.prototype.allPackageInfos = function(){
 
 };
 
+ServiceStore.prototype._commitRepo = function(){
+  // todo 
+
+};
+
 ServiceStore.prototype.exportToROS = function(package_name, service_meta, package_name){
-  return this.allPackageInfos().then(function(packages){ 
-    console.log(service_meta);
-    console.log(package_name);
-    var pack =  R.find(R.propEq('name', package_name))(packages);
+  return this.allPackageInfos()
+    .then(function(packages){ 
+      console.log(packages);
+
+      console.log(service_meta);
+      console.log(package_name);
+      console.log('here');
+
+      var pack = _.find(packages, {name: package_name});
+      console.log("PACK", pack);
 
 
-    var name_key = service_meta.name.replace(/\s+/g, "_").toLowerCase();
-    var service_base = Path.join( Path.dirname(pack.path), "services", name_key);
+      var name_key = service_meta.name.replace(/\s+/g, "_").toLowerCase();
+      var service_base = Path.join( Path.dirname(pack.path), "services", name_key);
 
 
-    var xml = fs.readFileSync(pack.path)
-    var xmlDoc = libxml.parseXmlString(xml);
+      var xml = fs.readFileSync(pack.path)
+      var xmlDoc = libxml.parseXmlString(xml);
 
-    var package = xmlDoc.get('/package');
+      var package = xmlDoc.get('/package');
 
-    var node = package.get('//export');
-    if(!node){
-      node = package.node('export');
-    }
+      var node = package.get('//export');
+      if(!node){
+        node = package.node('export');
+      }
 
-    node.node('concert_service', Path.join('services', name_key, name_key+'.service'));
+      node.node('concert_service', Path.join('services', name_key, name_key+'.service'));
 
-    var resultXml = xmlDoc.toString(true);
+      var resultXml = xmlDoc.toString(true);
 
-    fs.writeFileSync(pack.path, resultXml);
-
-
+      fs.writeFileSync(pack.path, resultXml);
 
 
-    console.log(service_base);
 
 
-    mkdirp.sync(service_base);
-
-    console.log(name_key);
+      console.log(service_base);
 
 
-    // .parameters
-    var params = R.compose(
-      R.tap(console.log),
-      R.fromPairs,
-      R.map(R.props(['key', 'value']))
-    )(service_meta.parameters);
-    var param_file_content = _to_colon_sep(params);
-    console.log('---------------- .interactions --------------------');
-    R.forEach(function(i){
-      i.parameters = R.fromPairs(R.map(R.values)(i.parameters));
-    })(service_meta.interactions);
+      mkdirp.sync(service_base);
+
+      console.log(name_key);
 
 
-    console.log(yaml.dump(service_meta.interactions));
-
-    console.log('---------------- .parameters --------------------');
-    console.log(param_file_content);
-
-    console.log('---------------- .launcher --------------------');
-    console.log(service_meta.launcher.launcher_body);
-
-    // .service
-    var service_kv = R.pickAll("name description author priority interactions parameters".split(/\s+/), service_meta);
-    service_kv.launcher_type = service_meta.launcher.launcher_type
-    service_kv.launcher = name_key + ".launcher";
-    // service_kv.icon = name_key + ".icon";
-    service_kv.interactions = name_key + ".interactions";
-    service_kv.parameters = name_key + ".parameters";
-    var service_file_content = _to_colon_sep(service_kv);
-    console.log('---------------- .service --------------------');
-    console.log(service_file_content);
+      // .parameters
+      var params = R.compose(
+        R.tap(console.log),
+        R.fromPairs,
+        R.map(R.props(['key', 'value']))
+      )(service_meta.parameters);
+      var param_file_content = _to_colon_sep(params);
+      console.log('---------------- .interactions --------------------');
+      R.forEach(function(i){
+        i.parameters = R.fromPairs(R.map(R.values)(i.parameters));
+      })(service_meta.interactions);
 
 
-    // save icon
+      console.log(yaml.dump(service_meta.interactions));
 
-    return Promise.all([
-      fs.writeFileAsync(service_base + "/" + name_key + ".parameters", param_file_content),
-      fs.writeFileAsync(service_base + "/" + name_key + ".launcher", service_meta.launcher.launcher_body),
-      fs.writeFileAsync(service_base + "/" + name_key + ".service", service_file_content),
-      fs.writeFileAsync(service_base + "/" + name_key + ".interactions", yaml.dump(service_meta.interactions))
-    ]);
+      console.log('---------------- .parameters --------------------');
+      console.log(param_file_content);
+
+      console.log('---------------- .launcher --------------------');
+      console.log(service_meta.launcher.launcher_body);
+
+      // .service
+      var service_kv = R.pickAll("name description author priority interactions parameters".split(/\s+/), service_meta);
+      service_kv.launcher_type = service_meta.launcher.launcher_type
+      service_kv.launcher = name_key + ".launcher";
+      // service_kv.icon = name_key + ".icon";
+      service_kv.interactions = name_key + ".interactions";
+      service_kv.parameters = name_key + ".parameters";
+      var service_file_content = _to_colon_sep(service_kv);
+      console.log('---------------- .service --------------------');
+      console.log(service_file_content);
 
 
-    return Promise.resolve(true);
-  });
+      // save icon
+
+      return Promise.all([
+        fs.writeFileAsync(service_base + "/" + name_key + ".parameters", param_file_content),
+        fs.writeFileAsync(service_base + "/" + name_key + ".launcher", service_meta.launcher.launcher_body),
+        fs.writeFileAsync(service_base + "/" + name_key + ".service", service_file_content),
+        fs.writeFileAsync(service_base + "/" + name_key + ".interactions", yaml.dump(service_meta.interactions))
+      ]);
+
+
+      return Promise.resolve(true);
+    })
+    .then(function(ok){
+
+    });
 
 
 
