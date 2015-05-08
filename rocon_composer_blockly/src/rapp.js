95a85c3377970c7e06b5fb724babd5c6f919c060
Eunsub Kim <eunsub@gmail.com>
2015-05-08 17:17:13 +0900

    rapp authoring

--- rocon_composer_blockly/src/rapp.js
+++ rocon_composer_blockly/src/rapp.js
@@ -1,4 +1,8 @@
-var _ = require('lodash');
+var _ = require('lodash'),
+  Promise = require('bluebird'),
+  glob = Promise.promisify(require('glob')),
+  fs = Promise.promisifyAll(require('fs')),
+  yaml = require('js-yaml');
 
 
 /*
@@ -10,7 +14,43 @@ var Rapp = function(options){
 };
 
 Rapp.prototype.save = function(data){
+
+  if(_.isEmpty(data.compatibility)){ delete data.compatibility; }
+  if(_.isEmpty(data.parent_name)){ delete data.parent_name; }
+
   all_thens = [];
+
+
+
+  var rapp_meta = _.pick(data, 'display', 'description', 'compatibility', 'parent_name');
+  if(!_.isEmpty(data.parameters)){
+    rapp_meta.parameters = data.name + ".parameters";
+    var params = _(data.parameters).indexBy('key').mapValues('value').value();
+    all_thens.push(fs.writeFileAsync(rapp_base + "/" + data.name + ".parameters",
+                                     yaml.dump(params)));
+
+  }
+
+
+  if(!_.isEmpty(data.interfaces)){
+    all_thens.push(fs.writeFileAsync(rapp_base + "/" + data.name + ".interfaces",
+                                     yaml.dump(data.interfaces)));
+    rapp_meta.interfaces = data.name + ".interfaces";
+
+  }
+
+  if(!_.isEmpty(data.launcher_body)){
+    all_thens.push(fs.writeFileAsync(rapp_base + "/" + data.name + ".launch",
+                                     yaml.dump(data.interfaces)));
+    rapp_meta.launch = data.name + ".launch";
+
+  }
+
+
+  all_thens.push(fs.writeFileAsync(rapp_base + "/" + data.name + ".rapp",
+                                   yaml.dump(rapp_meta)));
+
+
   all_thens = all_thens.concat([
     // fs.writeFileAsync(rapp_base + "/" + name_key + ".parameters", param_file_content),
     // fs.writeFileAsync(rapp_base + "/" + name_key + ".launch", service_meta.launcher_body),
@@ -26,3 +66,5 @@ Rapp.prototype.keyed_interfaces = function(data){
     .value();
 
 };
+
+
